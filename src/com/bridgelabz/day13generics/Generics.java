56c106a1a6c60cc8558d352bbad84e6908cea95b
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-01-30 11:21:00 +0530

    Added: Program to find Maximum float number

--- src/com/bridgelabz/day13generics/Generics.java
+++ src/com/bridgelabz/day13generics/Generics.java
@@ -1,27 +1,24 @@
 package com.bridgelabz.day13generics;
 
 public class Generics {
-    static void max_variables(Integer num1, Integer num2, Integer num3){
+    static void max_variables(Float num1, Float num2, Float num3){
         if(num1.compareTo(num2) > 0){
             if(num1.compareTo(num3) > 0){
-                System.out.println( num1 + " is Maximum of three numbers ");
+                System.out.println( num1 + " is Maximum of three float numbers ");
             }else {
-                System.out.println( num3 + " is Maximum of three numbers ");
+                System.out.println( num3 + " is Maximum of three float numbers ");
+            }
+        }else{
+            if(num2.compareTo(num3) > 0){
+                System.out.println( num2 + " is Maximum of three float numbers ");
+            }else{
+                System.out.println( num3 + " is Maximum of three float numbers ");
             }
         }
-
-        else{
-            if(num2.compareTo(num3) > 0){
-                System.out.println( num2 + " is Maximum of three numbers ");
-            }
-            else{
-                System.out.println( num3 + " is Maximum of three numbers ");
-            }
-         }
-}
+    }
 
     public static void main(String[] args) {
-        max_variables(76,90,34);
+        max_variables(5.8f, 7.3f,3.9f);
     }
 
 }
